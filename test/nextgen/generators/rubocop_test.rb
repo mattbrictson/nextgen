66fda03965b7b74df6b5df3d553a10984cb3468f
Matt Brictson <matt@mattbrictson.com>
2023-10-27 08:54:45 -0700

    Use rubocop-rails >= 2.22.0 for better Rails 7.1 support
    Before, rubocop-rails treated the Rails 7.1 `Rails.env.local?` as an
    unrecognized environment name, requiring custom configuration of
    `Rails/UnknownEnv` as a workaround.
    
    Now that rubocop-rails 2.22.0 is released, this workaround is no longer
    needed.
    
    This commit removes the workaround and ensures that `rubocop-rails` is
    added to the Gemfile with the `>= 2.22.0` version requirement.

---
+++ test/nextgen/generators/rubocop_test.rb
@@ -0,0 +1,35 @@
+require_relative "test_case"
+
+class Nextgen::Generators::RubocopTest < Nextgen::Generators::TestCase
+  destination File.join(Dir.tmpdir, "test_#{SecureRandom.hex(8)}")
+  setup :prepare_destination
+
+  setup do
+    %w[README.md Rakefile].each { FileUtils.touch Pathname.new(destination_root).join(_1) }
+    Pathname.new(destination_root).join("Gemfile").write(<<~GEMFILE)
+      source "https://rubygems.org"
+    GEMFILE
+  end
+
+  test "adds rubocop gems in development group, with version spec for rubocop-rails" do
+    apply_generator
+
+    assert_file "Gemfile", /#{Regexp.quote(<<~GEMFILE)}/
+      group :development do
+        gem "rubocop", require: false
+        gem "rubocop-performance", require: false
+        gem "rubocop-rails", ">= 2.22.0", require: false
+      end
+    GEMFILE
+  end
+
+  test "generates a .rubocop.yml file that requires additional plugins" do
+    apply_generator
+
+    assert_file ".rubocop.yml", /#{Regexp.quote(<<~YML)}/
+      require:
+        - rubocop-performance
+        - rubocop-rails
+    YML
+  end
+end
