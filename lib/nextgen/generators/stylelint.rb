53b1fe79f12f15d8bb07296e6bbcfbb81c7903e3
Matt Brictson <matt@mattbrictson.com>
2024-01-05 15:59:31 -0800

    Upgrade to Stylelint 16
    - Switch to latest versions of all recommended stylelint packages
    - Change stylelintrc file from CJS to ESM

--- lib/nextgen/generators/stylelint.rb
+++ lib/nextgen/generators/stylelint.rb
@@ -1,9 +1,9 @@
 say_git "Install stylelint"
 add_yarn_packages(
-  "stylelint@^15",
-  "stylelint-config-standard@^34",
-  "stylelint-declaration-strict-value@^1",
-  "stylelint-prettier@^4",
+  "stylelint",
+  "stylelint-config-standard",
+  "stylelint-declaration-strict-value",
+  "stylelint-prettier",
   "prettier",
   "npm-run-all",
   dev: true
@@ -14,7 +14,7 @@ add_package_json_scripts(
   lint: "npm-run-all lint:**",
   fix: "npm-run-all fix:**"
 )
-copy_file ".stylelintrc.cjs"
+copy_file ".stylelintrc.js"
 
 say_git "Add stylelint to default rake task"
 copy_file "lib/tasks/stylelint.rake"
