8cbdb65dbd0250b952d70425919a3b74c2be37ae
Matt Brictson <matt@mattbrictson.com>
2024-03-30 15:33:56 -0700

    Configure Dependabot to keep generated CI workflow up to date

--- lib/nextgen/generators/github_actions.rb
+++ lib/nextgen/generators/github_actions.rb
@@ -1,2 +1,3 @@
 gems = File.exist?("Gemfile") ? File.read("Gemfile").scan(/^\s*gem ["'](.+?)["']/).flatten : []
 template ".github/workflows/ci.yml.tt", context: binding
+copy_file ".github/dependabot.yml"
