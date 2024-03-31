4459bfce46d355e912b738da7a946e005879839b
Matt Brictson <matt@mattbrictson.com>
2024-03-30 18:29:51 -0700

    Wait for system testing to load before compiling Vite assets
    This speeds up running e.g. unit tests where compiling assets is not
    needed.

--- template/test/support/vite.rb
+++ template/test/support/vite.rb
@@ -1,5 +1,7 @@
-return if ViteRuby.config.auto_build
-
-# Compile assets once at the start of testing
-millis = Benchmark.ms { ViteRuby.commands.build }
-puts format("Built Vite assets (%.1fms)", millis)
+# Compile assets once at the start of system testing
+ActiveSupport.on_load(:action_dispatch_system_test_case) do
+  unless ViteRuby.config.auto_build
+    millis = Benchmark.ms { ViteRuby.commands.build }
+    puts format("Built Vite assets (%.1fms)", millis)
+  end
+end
