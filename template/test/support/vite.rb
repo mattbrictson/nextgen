57b10a7ff3d6cc431cecc8032231baa2c4de813d
Matt Brictson <matt@mattbrictson.com>
2024-03-30 19:16:45 -0700

    Allow non-system tests to optionally rely on vite build

--- template/test/support/vite.rb
+++
@@ -1,7 +0,0 @@
-# Compile assets once at the start of system testing
-ActiveSupport.on_load(:action_dispatch_system_test_case) do
-  unless ViteRuby.config.auto_build
-    millis = Benchmark.ms { ViteRuby.commands.build }
-    puts format("Built Vite assets (%.1fms)", millis)
-  end
-end
