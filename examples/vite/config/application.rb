ed7279cc367dc99ff93de1aecc190a624cf06f49
Matt Brictson <matt@mattbrictson.com>
2024-04-12 14:06:45 -0700

    Regenerate examples/vite

--- examples/vite/config/application.rb
+++ examples/vite/config/application.rb
@@ -24,7 +24,7 @@ module ViteExample
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "America/Los_Angeles"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
