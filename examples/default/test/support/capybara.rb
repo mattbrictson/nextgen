7506c3800e3d1b32e2fd60d1255639a3e117334d
Matt Brictson <matt@mattbrictson.com>
2024-07-13 17:57:54 -0700

    Regenerate examples/default

--- examples/default/test/support/capybara.rb
+++ examples/default/test/support/capybara.rb
@@ -1,9 +1,14 @@
-require "capybara/rails"
+# frozen_string_literal: true
 
-Capybara.configure do |config|
-  config.default_max_wait_time = 2
-  config.save_path = "tmp/screenshots"
-  config.enable_aria_label = true
-  config.server = :puma, {Silent: true}
-  config.test_id = "data-testid"
+ActiveSupport.on_load(:action_dispatch_system_test_case) do
+  require "capybara"
+  require "capybara/rails"
+  
+  Capybara.configure do |config|
+    config.default_max_wait_time = 2
+    config.save_path = "tmp/screenshots"
+    config.enable_aria_label = true
+    config.server = :puma, {Silent: true}
+    config.test_id = "data-testid"
+  end
 end
