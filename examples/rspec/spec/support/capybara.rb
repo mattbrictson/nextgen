551d0e59cb97e562a074821fdd829a0e08977ee2
Matt Brictson <matt@mattbrictson.com>
2024-07-13 17:55:51 -0700

    Regenerate examples/rspec

--- examples/rspec/spec/support/capybara.rb
+++ examples/rspec/spec/support/capybara.rb
@@ -1,10 +1,15 @@
-require "capybara/rails"
-require "capybara/rspec"
+# frozen_string_literal: true
 
-Capybara.configure do |config|
-  config.default_max_wait_time = 2
-  config.save_path = "tmp/screenshots"
-  config.enable_aria_label = true
-  config.server = :puma, {Silent: true}
-  config.test_id = "data-testid"
+ActiveSupport.on_load(:action_dispatch_system_test_case) do
+  require "capybara"
+  require "capybara/rails"
+    require "capybara/rspec"
+  
+  Capybara.configure do |config|
+    config.default_max_wait_time = 2
+    config.save_path = "tmp/screenshots"
+    config.enable_aria_label = true
+    config.server = :puma, {Silent: true}
+    config.test_id = "data-testid"
+  end
 end
--- examples/rspec/spec/support/system.rb
+++ examples/rspec/spec/support/system.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium,
