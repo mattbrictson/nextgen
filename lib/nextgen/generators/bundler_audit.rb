0208d76bf9db13d235ecc020e8ccaedc03c706c5
Matt Brictson <matt@mattbrictson.com>
2024-08-07 16:33:44 -0700

    Add bundle-audit to Rails 7.2-generated GitHub Actions workflow

--- lib/nextgen/generators/bundler_audit.rb
+++ lib/nextgen/generators/bundler_audit.rb
@@ -2,3 +2,25 @@
 
 install_gem "bundler-audit", group: :development, require: false
 binstub "bundler-audit"
+
+if File.exist?(".github/workflows/ci.yml")
+  say_git "Add bundle-audit job to CI workflow"
+  inject_into_file ".github/workflows/ci.yml", <<~YAML.gsub(/^/, "  "), after: /^jobs:\n/
+    scan_gems:
+      runs-on: ubuntu-latest
+
+      steps:
+        - name: Checkout code
+          uses: actions/checkout@v4
+
+        - name: Set up Ruby
+          uses: ruby/setup-ruby@v1
+          with:
+            ruby-version: .ruby-version
+            bundler-cache: true
+
+        - name: Scan for security vulnerabilities in Ruby dependencies
+          run: bundle exec bundle-audit check --update -v
+
+  YAML
+end
