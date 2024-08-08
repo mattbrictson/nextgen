bf7e3f27b60df3f561562bd27adbb2ff86ff0ac1
Matt Brictson <matt@mattbrictson.com>
2024-08-08 10:04:25 -0700

    Remove GitHub Actions generator (provided by Rails 7.2) (#85)

--- lib/nextgen/generators/github_actions.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-gems = File.exist?("Gemfile") ? File.read("Gemfile").scan(/^\s*gem ["'](.+?)["']/).flatten : []
-template ".github/workflows/ci.yml.tt", context: binding
-copy_file ".github/dependabot.yml"
