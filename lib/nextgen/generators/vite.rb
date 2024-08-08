c42f3b1ce103cf3ac130f2f943b7579137e8ffc7
Matt Brictson <matt@mattbrictson.com>
2024-08-07 17:26:40 -0700

    Add Node to Rails 7.2-generated CI job when Vite is enabled

--- lib/nextgen/generators/vite.rb
+++ lib/nextgen/generators/vite.rb
@@ -116,3 +116,19 @@ comment_lines "config/environments/development.rb", /^\s*config\.assets\./
 comment_lines "config/environments/production.rb", /^\s*config\.assets\./
 gsub_file "app/views/layouts/application.html.erb", /^.*<%= stylesheet_link_tag.*$/, ""
 remove_gem "sprockets-rails"
+
+if File.exist?(".github/workflows/ci.yml")
+  say_git "Add Node steps to CI workflow"
+  node_spec = File.exist?(".node-version") ? 'node-version-file: ".node-version"' : 'node-version: "lts/*"'
+  inject_into_file ".github/workflows/ci.yml", <<~YAML.gsub(/^/, "      "), before: /^\s+- name: Run tests/
+
+    - name: Set up Node
+      uses: actions/setup-node@v4
+      with:
+        #{node_spec}
+        cache: yarn
+
+    - name: Install Yarn packages
+      run: npx --yes ci
+  YAML
+end
