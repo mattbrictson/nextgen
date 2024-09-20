d615bef596a84d84b1445b0b6be6a150905dda9b
Matt Brictson <matt@mattbrictson.com>
2024-09-20 11:39:00 -0700

    Remove dotenv workaround now that kamal supports dotenv 3.x

--- lib/nextgen/generators/dotenv.rb
+++ lib/nextgen/generators/dotenv.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-# kamal is incompatible with the latest version of dotenv, so fall back
-# to using the legacy dotenv-rails gem in that case.
-if File.read("Gemfile.lock").match?(/\bkamal\b/)
-  install_gem "dotenv-rails", group: %i[development test]
-else
-  install_gem "dotenv", version: ">= 3.0", group: %i[development test]
-end
+install_gem "dotenv", version: ">= 3.0", group: %i[development test]
 copy_file ".env.sample"
 gitignore "/.env*", "!/.env.sample"
