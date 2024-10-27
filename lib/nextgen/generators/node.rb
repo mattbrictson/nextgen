718fe863e86420edcdfd5bc01041f43f94bc393b
Matt Brictson <matt@mattbrictson.com>
2024-10-26 21:10:04 -0700

    Increase Node requirement from 18 to 20 (current LTS) (#80)

--- lib/nextgen/generators/node.rb
+++ lib/nextgen/generators/node.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 copy_file "package.json" unless File.exist?("package.json")
-inject_into_file "README.md", "\n- Node 18 (LTS) or newer\n- Yarn 1.x (classic)", after: /^- Ruby.*$/
+inject_into_file "README.md", "\n- Node 20 (LTS) or newer\n- Yarn 1.x (classic)", after: /^- Ruby.*$/
 inject_into_file "README.md", "\nbrew install node\nbrew install yarn", after: /^brew install rbenv.*$/
 gitignore "node_modules/"
