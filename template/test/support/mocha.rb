badb3e643694bdfe56ae67e4915f0b757c6e12e7
Matt Brictson <matt@mattbrictson.com>
2024-12-23 12:14:23 -0600

    Fix mocha stubbing_method_on_nil deprecation warning (#146)
    Fixes the following warning in nextgen-generated projects that use the
    optional mocha generator:
    
    > Mocha deprecation warning at test/support/mocha.rb:6:in 'block in <top
    (required)>': `Mocha::Configuration#stubbing_method_on_nil=` is
    deprecated and will be removed in a future release. `nil` is frozen in
    Ruby >= v2.2 and Mocha will be dropping support for Ruby v2.1. At that
    point it won't be possible to stub methods on `nil` any more.

--- template/test/support/mocha.rb
+++ template/test/support/mocha.rb
@@ -5,6 +5,5 @@ require "mocha/minitest"
 # Reference: https://rubydoc.info/gems/mocha/Mocha/Configuration
 Mocha.configure do |config|
   config.strict_keyword_argument_matching = true
-  config.stubbing_method_on_nil = :prevent
   config.stubbing_non_existent_method = :prevent
 end
