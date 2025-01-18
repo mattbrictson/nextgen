3c7ac3364a5d74801f0a54736998c3e6aa0e1707
Matt Brictson <matt@mattbrictson.com>
2025-01-18 10:41:47 -0800

    Regenerate default example

--- examples/default/test/support/capybara.rb
+++ examples/default/test/support/capybara.rb
@@ -3,7 +3,7 @@
 ActiveSupport.on_load(:action_dispatch_system_test_case) do
   require "capybara"
   require "capybara/rails"
-  
+
   Capybara.configure do |config|
     config.default_max_wait_time = 2
     config.save_path = "tmp/screenshots"
