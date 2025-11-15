cde4426f8b172c96a996ad1b48a9cdd9e3caaf89
Robert Hopman <robert@bonaroo.nl>
2025-11-16 01:10:42 +0330

    Feature: improve performance erb_lint (#178)
    This will check only the changed files and improves performance. 
    
    All the places that need the `--cache` flag have it and the autocorrect
    commands don't have it (since they're incompatible).
    
    I've tested with rake and overcommit. It was not tested against the CI
    workflow.
    
    ---------
    
    Co-authored-by: Matt Brictson <matt@mattbrictson.com>

--- lib/nextgen/generators/erb_lint.rb
+++ lib/nextgen/generators/erb_lint.rb
@@ -4,6 +4,7 @@ say_git "Install erb_lint"
 install_gem "erb_lint", group: :development, require: false
 binstub "erb_lint"
 template ".erb_lint.yml.tt"
+gitignore "/.erb_lint_cache"
 
 say_git "Add erb_lint to default rake task"
 copy_file "lib/tasks/erb_lint.rake"
