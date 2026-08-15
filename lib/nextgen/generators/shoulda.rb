348adb82c142dadf345594f889ee8f02da310694
Matt Brictson <matt@mattbrictson.com>
2026-08-14 17:37:38 -0700

    Pin shoulda-context >= 3.0.0.rc1 for Rails 8.1 compatibility

--- lib/nextgen/generators/shoulda.rb
+++ lib/nextgen/generators/shoulda.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 say_git "Install shoulda gems"
-install_gem "shoulda-context", group: :test if minitest?
+install_gem "shoulda-context", version: ">= 3.0.0.rc1", group: :test if minitest?
 install_gem "shoulda-matchers", group: :test
 
 say_git "Include shoulda methods in tests"
